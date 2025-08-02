12f7eb72b5c3eb9268ad5fd52657d1eda1ab4a7f
asli-f <aslitaser@outlook.com>
2025-08-03 01:07:01 +0300

    export mount types from types module

--- shadowfs-core/src/types/mod.rs
+++ shadowfs-core/src/types/mod.rs
@@ -4,6 +4,7 @@ pub mod metadata;
 pub mod operations;
 pub mod directory;
 pub mod error;
+pub mod mount;
 
 // Re-export all types from submodules
 pub use path::ShadowPath;
@@ -11,3 +12,4 @@ pub use metadata::{FileType, FilePermissions, PlatformMetadata, FileMetadata};
 pub use operations::{FileHandle, OpenFlags, Bytes, FileOperation};
 pub use directory::DirectoryEntry;
 pub use error::{ShadowError, OperationResult};
+pub use mount::{MountOptions, MountOptionsBuilder, CacheConfig, OverrideConfig};
