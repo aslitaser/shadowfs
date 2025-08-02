e5b37952a31bf6c300a266ee27c9709f5d1de1e1
asli-f <aslitaser@outlook.com>
2025-08-03 00:15:06 +0300

    add shadowfs-ffi package

---
+++ shadowfs-ffi/src/lib.rs
@@ -0,0 +1,6 @@
+//! C API for ShadowFS
+//! 
+//! This crate provides C bindings for ShadowFS functionality,
+//! allowing integration with other programming languages.
+
+// Placeholder for FFI implementation
